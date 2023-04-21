e06aa33844da40eed8e19c78ec021099f9c3df98
kol <kol@work.com>
2023-04-21 15:35:05 +0100

    Adding S3 images.

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,12 +1,36 @@
-import flask
+import os
+from flask import Flask, render_template, request, redirect, send_file 
+from s3demo import list_files, download_file, upload_file
 import hostinfo
 
-app = flask.Flask(__name__)
-
+app = Flask(__name__)
+BUCKET="monday-gs-999"
+UPLOAD_FOLDER = "uploads"
 
 @app.route("/")
 def home():
-    return flask.render_template("home.html", hostinfo=hostinfo.get_info())
+    return render_template("home.html", hostinfo=hostinfo.get_info())
+
+@app.route("/storage")
+def storage():
+    contents = list_files("flaskdrive")
+    return render_template('storage.html', contents=contents)
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(os.path.join(UPLOAD_FOLDER, f.filename))
+        upload_file(f"uploads/{f.filename}", BUCKET)
+
+        return redirect("/storage")
+
+@app.route("/download/<filename>", methods=['GET'])
+def download(filename):
+    if request.method == 'GET':
+        output = download_file(filename, BUCKET)
+
+        return send_file(output, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
